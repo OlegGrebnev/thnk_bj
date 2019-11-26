d2f5f95ff2ba29249bc74d235d543c82356a609c
grebnevon <grebnevon@gmail.com>
2019-11-26 20:09:27 +0300

    add classes card, deck, player

---
+++ deck.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Deck
+  SUITS = %w[♠ ♥ ♦ ♣].freeze
+  TYPES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, [1, 11]].freeze
+
+  def initialize
+    @deck = generate
+  end
+
+  private
+
+  attr_reader :deck
+
+  def generate
+    SUITS.reduce([]) do |acc, suit|
+      acc + TYPES.map.with_index { |type, idx| Card.new(type, suit, VALUES[idx]) }
+    end.shuffle
+  end
+end
---
+++ player.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class Player
+
+  attr_accessor :score, :cards, :bank_amount
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @bank_amount = 100
+    @score = 0
+    @cards = []
+  end
+
+  def get_card(card, score)
+    cards << card
+    self.score += score
+  end
+
+  def show_cards
+    cards.map(&:face).join(', ')
+  end
+
+  def bet
+    self.bank_amount -= 10
+    10
+  end
+
+  def take_money(amount)
+    self.bank_amount += amount
+  end
+
+  def reset
+    self.score = 0
+    self.cards = []
+  end
+
+  def bank_zero?
+    bank_amount.zero?
+  end
+
+  def continue?
+    !bank_zero?
+  end
+end
